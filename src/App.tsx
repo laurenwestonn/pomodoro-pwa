f635fb181151ef1171f8e5397c32b26de4063fc3
Lauren <lauren.carter@codecomputerlove.com>
2024-02-08 08:52:46 +0000

    ✨ Implement state machine!
    Switching between states due to button taps. Timer works

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import PomodoroButton from './tsx/PomodoroButton';
-import { useState } from 'react';
-import Stopwatch from './tsx/Stopwatch';
+import { useEffect, useState } from 'react';
+
+export enum State {
+  initial,
+  focus,
+  break,
+  results
+}
 
 function App() {
-  const [isRunning, setIsRunning] = useState(false);
+  const [state, setState] = useState(State.initial);
+  const [history, setHistory] = useState<number[]>([]);
+  const [time, setTime] = useState(0);
 
-  function setTimerRunning() {
-    setIsRunning(true);
-  }
 
-  function setTimerNotRunning() {
-    setIsRunning(false);
-  }
+  useEffect(() => {
+    let intervalId: NodeJS.Timer;
+    if (state === State.focus) {
+      // incrementing time by 1 every second
+      intervalId = setInterval(() => setTime(time + 1), 1000);
+    }
+    if (state === State.break) {
+      // decrease time by 1 every second
+      intervalId = setInterval(() => setTime(time - 1), 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [state, time]);
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <h1>{isRunning ? "Running..." : "Paused"}</h1>
-        <Stopwatch
-          isRunning={isRunning}
-        />
+        <h1>Lauren's pomodoro app</h1>
+        <h3>state: {State[state]}</h3>
+        <h3>history: {history.length === 0 ? "empty" : history.join()}</h3>
+        <h3>time: {time}</h3>
+
         <PomodoroButton
-          isRunning={isRunning}
-          onClick={ isRunning ? setTimerNotRunning : setTimerRunning }
+          state={state}
+          setState={setState}
+          history={history}
+          setHistory={setHistory}
+          time={time}
+          setTime={setTime}
         />
 
         <img src={logo} className="App-logo" alt="logo" />
--- src/tsx/PomodoroButton.tsx
+++ src/tsx/PomodoroButton.tsx
@@ -1,19 +1,83 @@
 import '../styles/PomodoroButton.css';
+import {State} from '../App';
 
 interface PomodoroButtonInterface {
-  onClick: () => void,
-  isRunning: boolean,
+  state: State,
+  setState: (state: State) => void,
+  time: number,
+  setTime: (time: number) => void,
+  history: number[],
+  setHistory: (time: number[]) => void
+}
+
+const formatTime = (time: number) => {
+  const absTime = Math.abs(time);
+  const hours = Math.floor(absTime / 3600);
+  const minutes = Math.floor((absTime % 3600) / 60);
+  const seconds = Math.floor(absTime % 60);
+
+  if (time >= 0) {
+    return (
+      <>
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
+      </>
+    );
+  } else {
+    return (
+      <>
+        - {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
+      </>
+    );
+
+  }
+};
+
+const calcBreak = (time: number) => {
+  return Math.floor(time / 5);
 }
 
 const PomodoroButton = (props: PomodoroButtonInterface) => {
 
-
-  return (
-    <button
-      className="pomodoro-button"
-      onClick={props.onClick}>{props.isRunning ? 'Stop' : 'Start'}
-    </button>
-  )
+  if (props.state === State.initial) {
+    return (
+      <button
+        className="pomodoro-button pomodoro-button__initial"
+        onClick={() => props.setState(State.focus)}>
+          Start
+      </button>
+    )
+  } else if (props.state === State.focus) {
+    return (
+      <button
+        className="pomodoro-button pomodoro-button__focus"
+        onClick={() => {
+          props.setState(State.break);
+          props.setHistory([...props.history, props.time])
+          props.setTime(calcBreak(props.time));
+        }}>
+          <p>{formatTime(props.time)}</p>
+          <p>Tap for a break</p>
+      </button>
+    )
+  } else if (props.state === State.break) {
+    return (
+      <button
+        className="pomodoro-button pomodoro-button__break"
+        onClick={() => {
+          props.setState(State.focus);
+          const recommendedBreak = calcBreak(props.history[props.history.length-1]);
+          console.log('recommended break was ' + recommendedBreak + ", minus current time " + props.time + " = " + (recommendedBreak - props.time));
+          props.setHistory([...props.history, recommendedBreak - props.time])
+          props.setTime(0);
+        }}>
+          <p>Back to it in...</p>
+          <p>{formatTime(props.time)}</p>
+      </button>
+    )
+  }
+  return (<div>don't show pomo button</div>)
 }
 
 export default PomodoroButton;
