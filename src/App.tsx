22d5253aae1e95a335d9a78fa4ebc3466d7e8560
Lauren <lauren.carter@codecomputerlove.com>
2024-02-08 10:10:28 +0000

    💄 Button styling, add icons. Basic results view

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import reset from './img/reset.png';
+import finish from './img/finish.png';
 import './App.css';
 import PomodoroButton from './tsx/PomodoroButton';
 import { useEffect, useState } from 'react';
@@ -9,6 +10,43 @@ export enum State {
   break,
   results
 }
+
+export const formatTime = (time: number) => {
+  const absTime = Math.abs(time);
+  const hours = Math.floor(absTime / 3600);
+  const minutes = Math.floor((absTime % 3600) / 60);
+  const seconds = absTime % 60;
+
+
+    return (
+      <>
+        {time < 0 ? "-" : ""}
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
+      </>
+    );
+};
+
+const formatTimeSimple = (time: number) => {
+  const absTime = Math.abs(time);
+  if (absTime < 60) {
+    return `${absTime}s`
+  } else {
+    if (absTime < 3600) {
+      return `${Math.floor(absTime / 60)}m ${Math.floor(absTime % 60)}s`
+    }
+  }
+
+  const hours = Math.floor(absTime / 3600);
+  const minutes = Math.floor((absTime % 3600) / 60);
+  const seconds = absTime % 60;
+
+  return (
+    <>
+      {time >= 0 ? "-" : ""} {hours}hr {minutes.toString().padStart(2, "0")}m {seconds.toString().padStart(2, "0")}s
+    </>
+  );
+};
 
 function App() {
   const [state, setState] = useState(State.initial);
@@ -32,11 +70,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <h1>Lauren's pomodoro app</h1>
-        <h3>state: {State[state]}</h3>
+        {/* <h3>state: {State[state]}</h3>
         <h3>history: {history.length === 0 ? "empty" : history.join()}</h3>
-        <h3>time: {time}</h3>
+        <h3>time: {time}</h3> */}
 
         <PomodoroButton
           state={state}
@@ -47,18 +83,56 @@ function App() {
           setTime={setTime}
         />
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {(
+          state === State.results
+        ) && (
+          <>
+            <h1>Results</h1>
+            {history.map((time, i) => (
+              <p key={i} className={i % 2 === 0 ? "time__focus" : "time__break"}>
+                {formatTimeSimple(time)}
+              </p>
+            ))}
+          </>
+        )}
+
+        {(
+          state === State.focus ||
+          state === State.break ||
+          state === State.results
+        ) && (
+          <button
+            className="circle-button"
+            aria-label="reset"
+            // style={{backgroundImage: logo}}
+            style={{backgroundImage: `url(${reset})`, backgroundRepeat: "no-repeat", backgroundPosition: "50% 50%", backgroundSize: "70%" }}
+            // style={{backgroundImage: './logo.svg'}}
+            onClick={() => {
+              setState(State.initial);
+              setHistory([]);
+              setTime(0);
+            }}
+          >
+          </button>
+        )}
+
+        {(
+          state === State.focus ||
+          state === State.break
+        ) && (
+          <button
+            className="circle-button"
+            aria-label="finish"
+            style={{backgroundImage: `url(${finish})`, backgroundRepeat: "no-repeat", backgroundPosition: "50% 50%", backgroundSize: "70%" }}
+            onClick={() => {
+              setState(State.results);
+              setHistory([...history, time]);
+            }}
+          >
+          </button>
+        )}
+
+
       </header>
     </div>
   );
--- src/tsx/PomodoroButton.tsx
+++ src/tsx/PomodoroButton.tsx
@@ -1,5 +1,5 @@
 import '../styles/PomodoroButton.css';
-import {State} from '../App';
+import {State, formatTime} from '../App';
 
 interface PomodoroButtonInterface {
   state: State,
@@ -9,30 +9,6 @@ interface PomodoroButtonInterface {
   history: number[],
   setHistory: (time: number[]) => void
 }
-
-const formatTime = (time: number) => {
-  const absTime = Math.abs(time);
-  const hours = Math.floor(absTime / 3600);
-  const minutes = Math.floor((absTime % 3600) / 60);
-  const seconds = Math.floor(absTime % 60);
-
-  if (time >= 0) {
-    return (
-      <>
-        {hours}:{minutes.toString().padStart(2, "0")}:
-        {seconds.toString().padStart(2, "0")}
-      </>
-    );
-  } else {
-    return (
-      <>
-        - {hours}:{minutes.toString().padStart(2, "0")}:
-        {seconds.toString().padStart(2, "0")}
-      </>
-    );
-
-  }
-};
 
 const calcBreak = (time: number) => {
   return Math.floor(time / 5);
@@ -45,7 +21,7 @@ const PomodoroButton = (props: PomodoroButtonInterface) => {
       <button
         className="pomodoro-button pomodoro-button__initial"
         onClick={() => props.setState(State.focus)}>
-          Start
+          <p>Start</p>
       </button>
     )
   } else if (props.state === State.focus) {
@@ -57,8 +33,7 @@ const PomodoroButton = (props: PomodoroButtonInterface) => {
           props.setHistory([...props.history, props.time])
           props.setTime(calcBreak(props.time));
         }}>
-          <p>{formatTime(props.time)}</p>
-          <p>Tap for a break</p>
+          <p><span>{formatTime(props.time)}</span><br />Tap for a break</p>
       </button>
     )
   } else if (props.state === State.break) {
@@ -68,16 +43,15 @@ const PomodoroButton = (props: PomodoroButtonInterface) => {
         onClick={() => {
           props.setState(State.focus);
           const recommendedBreak = calcBreak(props.history[props.history.length-1]);
-          console.log('recommended break was ' + recommendedBreak + ", minus current time " + props.time + " = " + (recommendedBreak - props.time));
           props.setHistory([...props.history, recommendedBreak - props.time])
           props.setTime(0);
         }}>
-          <p>Back to it in...</p>
-          <p>{formatTime(props.time)}</p>
+          <p>Back to it in...<br /><span>{formatTime(props.time)}</span></p>
       </button>
     )
   }
-  return (<div>don't show pomo button</div>)
+
+  return null;
 }
 
 export default PomodoroButton;
